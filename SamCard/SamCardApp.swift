c3b87af8fa53d4e9d3207b1b56a6fb96e98ef61d
Samarah Anderson <samarahtanderson@gmail.com>
2023-06-08 19:01:01 -0400

    Initial Commit

---
+++ SamCard/SamCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SamCardApp.swift
+//  SamCard
+//
+//  Created by Samarah Anderson on 6/8/23.
+//
+
+import SwiftUI
+
+@main
+struct SamCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
